28f935ea1577319564fd4f2d8ceeb0ef3ba4df54
Marszy <fzyuan0529@gmail.com>
2023-08-17 19:52:06 +0800

    提交信息

--- scripts/gen-rss.mjs
+++ scripts/gen-rss.mjs
@@ -1,44 +1,38 @@
-import { promises as fs } from 'fs'
-import matter from 'gray-matter'
-import { join } from 'path'
-import RSS from 'rss'
-
-// Message definition
-const msgError = '\x1b[0m[\x1b[31m ERROR \x1b[0m]'
-const msgDone = '\x1b[0m[\x1b[32m DONE \x1b[0m]'
-// const msgInfo = '\x1b[0m[\x1b[33m INFO \x1b[0m]'
-// const msgWarn = '\x1b[0m[\x1b[33m WARN \x1b[0m]'
+const { promises: fs } = require('fs')
+const path = require('path')
+const RSS = require('rss')
+const matter = require('gray-matter')
 
 async function generate() {
-  try {
-    const feed = new RSS({
-      title: "Marszy's Blog",
-      site_url: 'https://loveur.life',
-      feed_url: 'https://loveur.life/feed.xml',
-    })
+  const feed = new RSS({
+    title: 'Marszy',
+    site_url: 'https://loveur.life',
+    feed_url: 'https://loveur.life/feed.xml',
+  })
 
-    const dirPath = join('./pages/posts')
-    const posts = await fs.readdir(dirPath)
+  const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
 
-    for (const post of posts.filter(fileName => !fileName.startsWith('index.'))) {
-      const content = await fs.readFile(join(dirPath, post))
+  await Promise.all(
+    posts.map(async (name) => {
+      if (name.startsWith('index.')) return
+
+      const content = await fs.readFile(
+        path.join(__dirname, '..', 'pages', 'posts', name)
+      )
       const frontmatter = matter(content)
 
       feed.item({
         title: frontmatter.data.title,
-        url: `/posts/${post.replace(/\.mdx?/, '')}`,
+        url: '/posts/' + name.replace(/\.mdx?/, ''),
         date: frontmatter.data.date,
         description: frontmatter.data.description,
+        categories: frontmatter.data.tag.split(', '),
         author: frontmatter.data.author,
       })
-    }
+    })
+  )
 
-    await fs.writeFile('./public/feed.xml', feed.xml({ indent: true }))
-
-    console.log(msgDone, 'RSS feed generated successfully!')
-  } catch (error) {
-    console.error(msgError, 'Failed to generate RSS feed:', error)
-  }
+  await fs.writeFile('./public/feed.xml', feed.xml({ indent: true }))
 }
 
 generate()
