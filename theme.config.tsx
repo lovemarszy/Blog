b2d31bc33132ba337ac07f595c224236f08669d0
Marszy <fzyuan0529@gmail.com>
2023-08-17 17:51:48 +0800

    提交信息

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,34 +1,20 @@
-import CustomHead from '#components/custom-head'
-import Link from 'next/link'
-import { FaRss } from 'react-icons/fa'  // 只保留 RSS 图标
-
 const YEAR = new Date().getFullYear()
 
-const NextraThemeConfig = {
-  head: CustomHead,
-
-  dateFormatter: (date: Date) => `${date.toDateString()}`,
-
+export default {
   footer: (
-    <div>
-      <hr />
-      <div className="grid auto-cols-min grid-flow-col gap-8 text-xl ss:gap-4">
-        <Link href="/feed.xml" target="_blank" rel="noreferrer" aria-label="RSS" className="">
-          <FaRss />
-        </Link>
-      </div>
-      <small className="mt-32 block text-p-light dark:text-inherit">
-        <abbr
-          title="This site and all its content are licensed under a Attribution-NonCommercial-ShareAlike 4.0 International License."
-          className="cursor-help"
-        >
-          CC BY-NC-SA 4.0
-        </abbr>{' '}
+    <footer>
+      <small>
         <time>{YEAR}</time> © Marszy.
-        <div className="float-right">[ Marszy ]</div>
+        <a href="/feed.xml">RSS</a>
       </small>
-    </div>
+      <style jsx>{`
+        footer {
+          margin-top: 8rem;
+        }
+        a {
+          float: right;
+        }
+      `}</style>
+    </footer>
   ),
 }
-
-export default NextraThemeConfig
