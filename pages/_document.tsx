3c8715ee4a5be4ad363ad88189797fbddd4358df
Marszy <fzyuan0529@gmail.com>
2023-08-17 20:36:41 +0800

    提交信息

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="cn">
+    <Html lang="zh-CN">
       <Head />
       <body className="bg-back-light tracking-custom antialiased selection:bg-selected dark:bg-back-dark ss:min-h-dynamic">
         <Main />
